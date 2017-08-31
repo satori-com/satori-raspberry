0c82f439018eced04f0b99279ad05dceaac7ae28
Aleksandr Zinovev <azinoviev@satori.com>
2017-08-31 12:04:49 +0200

    setup.py: Update setup requirements.

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@
 
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as r:
-    lines = [line for line in r.read().splitlines() if line.strip() != '']
-    reqs = [req for req in lines if not req.startswith('git+')]
-    deps = [dep for dep in lines if dep.startswith('git+')]
+requires = [
+    'RPi.GPIO==0.6.3',
+    'satori-rtm-sdk==1.3.0',
+    'Adafruit_DHT>=1.3.2',
+    'smbus2==0.2.0'
+]
+
+dependencies = [
+    'git+https://github.com/adafruit/Adafruit_Python_DHT#egg=Adafruit_DHT-1.3.2'
+]
 
 classifiers = [
     'Development Status :: 4 - Beta',
@@ -24,8 +30,8 @@ setup(
     author_email='sdk@satori.com',
     url='https://github.com/satori-com/satori-raspberry',
     packages=find_packages(exclude=['examples']),
-    install_requires=reqs,
-    dependency_links=deps,
+    install_requires=requires,
+    dependency_links=dependencies,
     classifiers=classifiers,
     license='Proprietary',
     keywords=['satori', 'raspberry', 'iot'],
